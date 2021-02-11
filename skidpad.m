bd3de2c0d3552b25ab8e695ebc8896fe6753429f
Obailey00 <70101593+Obailey00@users.noreply.github.com>
2021-02-11 23:50:50 +0000

    Update skidpad.m

--- skidpad.m
+++ skidpad.m
@@ -4,6 +4,8 @@
 iter = 10; % number of iteration for velocity covnergence
 v = zeros(1,iter); %velocity vector
 v(1) = 10; %first guess of 10 m/s
+
+
 cl = 3.5;
 hs = 0.27033;
 m = 294;
@@ -13,6 +15,8 @@ s = 1.1;
 %cdh = range of cg heights (added in later)
 twf = 1:0.01:1.5;
 twr = 1:0.01:1.5;
+r = zeros(length(twf),length(twr));
+V = zeros(length(twf),length(twr));
 
 
 
@@ -28,35 +32,47 @@ twr = 1:0.01:1.5;
       r(j,k) = 7.625 + twr(k)/2;
      elseif twr(j) == twf(k)
        r(j,k) = 7.625 + twf(j)/2;
-end
+  end
 
-skidpad_distance = 2*pi*r; %distance of the skidpad
-ay = v(i)^2/(r(j,k)*g);
+    skidpad_distance(j,k) = 2*pi*r(j,k); %distance of the skidpad
+    ay = v(i)^2/(r(j,k)*g);
 
-%perpendicular distance funtion goes here
+    %perpendicular distance funtion goes here when varying cg height comes
 
-load_transfer_front = (Ws*ay/twf(j))*(hs*((Kphif + Ws*hs*(1-weightdist))/(Kphif+Kphir-Ws*hs)) + (1-weightdist)*Zrcf) + (Wuf*UScg*ay)/twf(j);
-load_transfer_rear  = (Ws*ay/twr(k))*(hs*((Kphir + Ws*hs*weightdist)/(Kphif+Kphir-Ws*hs)) + weightdist*Zrcr) + (Wur*UScg*ay)/twr(k);
+   load_transfer_front = (Ws*ay/twf(j))*(hs*((Kphif + Ws*hs*(1-weightdist))/(Kphif+Kphir-Ws*hs)) + (1-weightdist)*Zrcf) + (Wuf*UScg*ay)/twf(j);
+   load_transfer_rear  = (Ws*ay/twr(k))*(hs*((Kphir + Ws*hs*weightdist)/(Kphif+Kphir-Ws*hs)) + weightdist*Zrcr) + (Wur*UScg*ay)/twr(k);
 
-Fz_fstatic = (m*g*weightdist)/2; %static normal loads
-Fz_rstatic = (m*g*(1-weightdist))/2;
+   Fz_fstatic = (m*g*weightdist)/2; %static normal loads
+   Fz_rstatic = (m*g*(1-weightdist))/2;
 
-df(i) = 0.5*rho*s*cl*v(i)^2; %calculate down force
+   df(i) = 0.5*rho*s*cl*v(i)^2; %calculate down force
 
-Fz_fl = Fz_fstatic + load_transfer_front + df(i)/4; %loads (ask for aero balance)
-Fz_fr = Fz_fstatic - load_transfer_front + df(i)/4;
-Fz_rl = Fz_rstatic + load_transfer_front + df(i)/4;
-Fz_rr = Fz_rstatic - load_transfer_front + df(i)/4;
+   Fz_fl = Fz_fstatic + load_transfer_front + df(i)/4; %loads (ask for aero balance)
+   Fz_fr = Fz_fstatic - load_transfer_front + df(i)/4;
+   Fz_rl = Fz_rstatic + load_transfer_rear + df(i)/4;
+   Fz_rr = Fz_rstatic - load_transfer_rear + df(i)/4;
 
-Fy_fl(i) = (1.95 +0.0001*(675 - Fz_fl))*Fz_fl;
-Fy_fr(i) = (1.95 +0.0001*(675 - Fz_fr))*Fz_fr;
-Fy_rl(i) = (1.95 +0.0001*(675 - Fz_rl))*Fz_rl;
-Fy_rr(i) = (1.95 +0.0001*(675 - Fz_rr))*Fz_rr;
-NFY(i) = (Fy_fl(i) + Fy_fr(i) + Fy_rl(i) + Fy_rr(i))/(m*g + df(i)); %calculate average NFY
-
-v(i+1) = sqrt((m*g)/((m/(NFY(i)*r(j,k)))-0.5*rho*s*cl)); %calculate new speed
-Skidpad_time = skidpad_distance/v(end);
+   Fy_fl(i) = (1.95 +0.0001*(675 - Fz_fl))*Fz_fl;
+   Fy_fr(i) = (1.95 +0.0001*(675 - Fz_fr))*Fz_fr;
+   Fy_rl(i) = (1.95 +0.0001*(675 - Fz_rl))*Fz_rl;
+   Fy_rr(i) = (1.95 +0.0001*(675 - Fz_rr))*Fz_rr;
+   
+   v(i+1) = sqrt((r(j,k)*(Fy_fl(i) + Fy_fr(i) + Fy_rl(i) + Fy_rr(i)))/m);
+   
+   
+   if i == 10
+       V(j,k) = v(end); %place converged velcoity in an array
+       v = zeros(1,iter); %reset velocity vector for next iteration
+       v(1) = 10; %initial velocity of 10 m/s
+   end
+      
         end
     end
 end
+skidpad_time = skidpad_distance./V;
 
+contourf(twf,twf,skidpad_time)
+colorbar
+xlabel('front track width')
+ylabel('read track width')
+title('skidpad time')
