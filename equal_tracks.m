b8b05aaca8e03f6883bc6a6d1236e001b6a61e6d
Murray Macfarlane <murraymacf@gmail.com>
2021-02-23 22:15:57 +0000

    fixed ur intentation

--- equal_tracks.m
+++ equal_tracks.m
@@ -1,73 +1,79 @@
 %skidpad track width simulation
 %assume the car is turning right
 %basic convergence, assume a velocity of 10 m/s and iterate%
+clear all;
+load USMEV01Rates&Parameters.mat;
+
 iter = 10; % number of iteration for velocity covnergence
 v = zeros(1,iter); %velocity vector
 v(1) = 10; %first guess of 10 m/s
 
 
-cl = 3.5;
+BaseCl = 3.5;
 hs = 0.27033;
 m = 294;
 g = 9.81;
 rho = 1.225;
 s = 1.1;
 
+TWScalingFactor = 1;
+
 %cdh = range of cg heights (added in later)
-tw = 0.5:0.001:3;
+tw = 1:0.001:2;
 twmm = tw.*1000;
 ntw = size(tw,2);
 minr = 3;
-maxr = 50;
+maxr = 20;
 corner_radius = minr:(maxr-minr)/ntw:maxr;
 %corner_radius = corner_radius(1:length(tw)); %janky fuckery to be able to plot, need changed
 r = zeros(length(tw),length(corner_radius));
 distance = zeros(length(tw),length(corner_radius)); % array for storing lengths
 
 
- for j = 1:length(tw)
-   for k = 1:length(corner_radius)
-       for i = 1:length(v)
-    
+for j = 1:length(tw)
+	cl = BaseCl + (tw(j)-1.2)*TWScalingFactor;
+	for k = 1:length(corner_radius)
+		for i = 1:length(v)
 
-%calculating radius that the car will follow around the skidpad%
-  r(j,k) = corner_radius(k);
 
-    skidpad_distance(j,k) = 2*pi*r(j,k); %distance of the skidpad
-    ay = v(i)^2/(r(j,k)*g);
+			%calculating radius that the car will follow around the skidpad
+			r(j,k) = corner_radius(k);
 
-    %perpendicular distance funtion goes here when varying cg height comes
-   %calculating load transfers
-   load_transfer_front = (Ws*ay/tw(j))*(hs*((Kphif + Ws*hs*(1-weightdist))/(Kphif+Kphir-Ws*hs)) + (1-weightdist)*Zrcf) + (Wuf*UScg*ay)/tw(j);
-   load_transfer_rear  = (Ws*ay/tw(j))*(hs*((Kphir + Ws*hs*weightdist)/(Kphif+Kphir-Ws*hs)) + weightdist*Zrcr) + (Wur*UScg*ay)/tw(j);
+			skidpad_distance(j,k) = 2*pi*r(j,k); %distance of the skidpad
+			ay = v(i)^2/(r(j,k)*g);
 
-   Fz_fstatic = (m*g*weightdist)/2; %static normal loads
-   Fz_rstatic = (m*g*(1-weightdist))/2;
+			%perpendicular distance funtion goes here when varying cg height comes
+			%calculating load transfers
+			load_transfer_front = (Ws*ay/tw(j))*(hs*((Kphif + Ws*hs*(1-weightdist))/(Kphif+Kphir-Ws*hs)) + (1-weightdist)*Zrcf) + (Wuf*UScg*ay)/tw(j);
+			load_transfer_rear  = (Ws*ay/tw(j))*(hs*((Kphir + Ws*hs*weightdist)/(Kphif+Kphir-Ws*hs)) + weightdist*Zrcr) + (Wur*UScg*ay)/tw(j);
 
-   df = 0.5*rho*s*cl*v(i)^2; %calculate down force
+			Fz_fstatic = (m*g*weightdist)/2; %static normal loads
+			Fz_rstatic = (m*g*(1-weightdist))/2;
 
-   Fz_fl = Fz_fstatic + load_transfer_front + df/4; %loads (ask for aero balance)
-   Fz_fr = Fz_fstatic - load_transfer_front + df/4;
-   Fz_rl = Fz_rstatic + load_transfer_rear + df/4;
-   Fz_rr = Fz_rstatic - load_transfer_rear + df/4;
+			df = 0.5*rho*s*cl*v(i)^2; %calculate down force
 
-   Fy_fl = (1.95 +0.0001*(675 - Fz_fl))*Fz_fl; %calculting tyre forces
-   Fy_fr = (1.95 +0.0001*(675 - Fz_fr))*Fz_fr;
-   Fy_rl = (1.95 +0.0001*(675 - Fz_rl))*Fz_rl;
-   Fy_rr = (1.95 +0.0001*(675 - Fz_rr))*Fz_rr;
-   
-   v(i+1) = sqrt((r(j,k)*(Fy_fl + Fy_fr + Fy_rl + Fy_rr))/m); % calculating velocity
-   %tyre forces = centripetal acc
-   
-   if i == iter
-       V(j,k) = v(end); %place converged velcoity in an array
-       v = zeros(1,iter); %reset velocity vector for next iteration
-       v(1) = 10; %initial velocity of 10 m/s
-   end
-      
-        end
-    end
- end
+			Fz_fl = Fz_fstatic + load_transfer_front + df/4; %loads (ask for aero balance)
+			Fz_fr = Fz_fstatic - load_transfer_front + df/4;
+			Fz_rl = Fz_rstatic + load_transfer_rear + df/4;
+			Fz_rr = Fz_rstatic - load_transfer_rear + df/4;
+
+			Fy_fl = (1.95 +0.0001*(675 - Fz_fl))*Fz_fl; %calculting tyre forces
+			Fy_fr = (1.95 +0.0001*(675 - Fz_fr))*Fz_fr;
+			Fy_rl = (1.95 +0.0001*(675 - Fz_rl))*Fz_rl;
+			Fy_rr = (1.95 +0.0001*(675 - Fz_rr))*Fz_rr;
+
+			v(i+1) = sqrt((r(j,k)*(Fy_fl + Fy_fr + Fy_rl + Fy_rr))/m); % calculating velocity
+			%tyre forces = centripetal acc
+
+			if i == iter
+				V(j,k) = v(end); %place converged velcoity in an array
+				v = zeros(1,iter); %reset velocity vector for next iteration
+				v(1) = 10; %initial velocity of 10 m/s
+			end
+
+		end
+	end
+end
  
 skidpad_time = skidpad_distance./V;
 
@@ -77,3 +83,10 @@ colorbar
 xlabel('Corner Radius (m)')
 ylabel('Track Width (mm)')
 title('Skidpad Time (s)')
+
+figure()
+contourf(corner_radius,twmm,V,20)
+colorbar
+xlabel('Corner Radius (m)')
+ylabel('Track Width (mm)')
+title('Maximum Velocity (m/s)')
